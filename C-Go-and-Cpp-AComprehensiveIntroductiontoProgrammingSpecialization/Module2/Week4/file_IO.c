c5da847af96023ddf3fca579083f82c319be8488
Khaled Houda <k.houda@tu-braunschweig.de>
2026-04-09 19:18:07 +0200

    fileIO upload

---
+++ C-Go-and-Cpp-AComprehensiveIntroductiontoProgrammingSpecialization/Module2/Week4/file_IO.c
@@ -0,0 +1,20 @@
+//
+// Created by khaled on 08.04.26.
+//
+
+#include <stdio.h>
+
+#define MAX_HW 20
+
+
+void read_data(FILE *ptr, int d[], int *size){
+    *size = 0;
+    while( fscanf(ptr, "%d", (&d[*size])) ==1 ){
+
+
+
+
+    }
+
+
+}
